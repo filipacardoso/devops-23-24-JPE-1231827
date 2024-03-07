1bba1303ad46ef3fa9cf1b0ba692e431d64016f4
Filipa Cardoso <1231827@isep.ipp.pt>
2024-03-07 14:56:19 +0000

    #1 added test to class EmployeeTest

--- CA1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
+++ CA1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -32,6 +32,99 @@ public class EmployeeTest {
         assertEquals(expectedId, foundID);
     }
 
+    @Test
+    public void getFirstName_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        String expectedName = "Frodo";
+        //act
+        String foundName = employee.getFirstName();
+        //assert
+        assertEquals(expectedName, foundName);
+    }
+
+    @Test
+    public void setFirstName_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        String expectedName = "Bilbo";
+        //act
+        employee.setFirstName(expectedName);
+        String foundName = employee.getFirstName();
+        //assert
+        assertEquals(expectedName, foundName);
+    }
+
+    @Test
+    public void getLastName_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        String expectedName = "Baggins";
+        //act
+        String foundName = employee.getLastName();
+        //assert
+        assertEquals(expectedName, foundName);
+    }
+
+    @Test
+    public void setLastName_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        String expectedName = "Bilbo";
+        //act
+        employee.setLastName(expectedName);
+        String foundName = employee.getLastName();
+        //assert
+        assertEquals(expectedName, foundName);
+    }
+
+    @Test
+    public void getDescription_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        String expectedDescription = "ring bearer";
+        //act
+        String foundDescription = employee.getDescription();
+        //assert
+        assertEquals(expectedDescription, foundDescription);
+    }
+
+    @Test
+    public void setDescription_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        String expectedDescription = "Bilbo";
+        //act
+        employee.setDescription(expectedDescription);
+        String foundDescription = employee.getDescription();
+        //assert
+        assertEquals(expectedDescription, foundDescription);
+    }
+
+    @Test
+    public void getJobYears_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        int expectedJobYears = 1;
+        //act
+        int foundJobYears = employee.getJobYears();
+        //assert
+        assertEquals(expectedJobYears, foundJobYears);
+    }
+
+    @Test
+    public void setJobYears_validEmployeeConstructor() {
+        //arrange
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1);
+        int expectedJobYears = 2;
+        //act
+        employee.setJobYears(expectedJobYears);
+        int foundJobYears = employee.getJobYears();
+        //assert
+        assertEquals(expectedJobYears, foundJobYears);
+    }
+
+
 
 
 
