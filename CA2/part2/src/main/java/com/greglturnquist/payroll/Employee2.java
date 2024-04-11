4248328edc11481660be47c9c3580bbc335e240e
Filipa Cardoso <1231827@isep.ipp.pt>
2024-04-11 16:18:19 +0100

    close #14  copy basic folder content to CA2/part2 folder

---
+++ CA2/part2/src/main/java/com/greglturnquist/payroll/Employee2.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.payroll;
+
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@Entity // <1>
+public class Employee2 {
+
+	private @Id @GeneratedValue Long id; // <2>
+	private String firstName;
+	private String lastName;
+	private String description;
+
+	private int jobYears;
+
+	private String email;
+
+	private Employee2() {}
+
+	public Employee2(String firstName, String lastName, String description, int jobYears, String email) throws InstantiationException {
+		if (!isValidConstructorArgument(firstName, lastName, description, jobYears, email) || !isValidEmail(email))
+			throw new InstantiationException("Invalid arguments");
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+		this.jobYears = jobYears;
+		this.email = email;
+	}
+
+
+	private boolean isValidConstructorArgument(String firstName, String lastName, String description, int jobYears, String email) {
+		 return firstName != null && !firstName.isEmpty() &&
+				 lastName != null && !lastName.isEmpty() &&
+				 description != null && !description.isEmpty() &&
+				 jobYears > 0 &&
+				 email != null && !email.isEmpty();
+	}
+
+	//Adapted from: https://emaillistvalidation.com/blog/mastering-email-validation-in-java-expert-techniques-and-best-practices/
+	public boolean isValidEmail(String email) {
+		String regex = "^[A-Za-z0-9+_.-]+@(.+)\\..+$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Employee2 employee = (Employee2) o;
+		return Objects.equals(id, employee.id) &&
+			Objects.equals(firstName, employee.firstName) &&
+			Objects.equals(lastName, employee.lastName) &&
+			Objects.equals(description, employee.description) &&
+			jobYears== employee.jobYears;
+
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(id, firstName, lastName, description);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getJobYears() {
+		return jobYears;
+	}
+
+	public void setJobYears(int jobYears) {
+		this.jobYears = jobYears;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		if (!isValidEmail(email)){
+			throw new IllegalArgumentException("Invalid email");
+		}
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+			"id=" + id +
+			", firstName='" + firstName + '\'' +
+			", lastName='" + lastName + '\'' +
+			", description='" + description + '\'' +
+			", jobYears='" + jobYears + '\'' +
+			'}';
+	}
+}
+// end::code[]
