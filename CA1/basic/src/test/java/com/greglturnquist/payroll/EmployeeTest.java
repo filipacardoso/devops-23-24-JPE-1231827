6756777d386c65fb4958544c095e7b4d72fa873a
Filipa Cardoso <1231827@isep.ipp.pt>
2024-06-13 15:38:49 +0100

    #28 updated Jenkinsfile file to CA5-part1

--- CA1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
+++ CA1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EmployeeTest {
 
